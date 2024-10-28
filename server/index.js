8cf8a99a8afcb5e7de694fc9d282187cf122edad
Amit Gupta <72860177+amitgupta-exe@users.noreply.github.com>
2024-10-28 22:08:42 +0530

    deploying on render 1

--- server/index.js
+++ server/index.js
@@ -16,7 +16,27 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://amit:1234@cluster0.t3tw5.mongodb.net/test';
-const PORT = 9001;
+
+const express = require("express");
+const path = require("path");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+// API routes
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+
 
 mongoose.set('strictQuery', true);
 
